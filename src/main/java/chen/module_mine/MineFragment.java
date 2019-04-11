a294a9a02f6b48f6ec404e4938a99fa8e1c78458
chanzhilin <1181785848@qq.com>
2019-04-11 15:33:10 +0800

    create base frame

---
+++ src/main/java/chen/module_mine/MineFragment.java
@@ -0,0 +1,45 @@
+package chen.module_mine;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class MineFragment extends Fragment {
+    private static final String ARG_PARAM1 = "param1";
+
+    private String mParam1;
+
+    private TextView mTextView;
+
+
+    public static MineFragment newInstance(String param1) {
+        MineFragment fragment = new MineFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_mine, container, false);
+        mTextView = view.findViewById(R.id.textView);
+        mTextView.setText(mParam1 + "_fragment");
+        return view;
+    }
+
+}
